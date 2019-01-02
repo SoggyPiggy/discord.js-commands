93040ca79cf44cb817fd8720f26b74086c29595d
SoggyPiggy <soggypiggy@gmail.com>
2019-01-02 08:57:36 -0500

    Update commandregistry.js added group and command registers and creators

--- src/structures/commandregistry.js
+++ src/structures/commandregistry.js
@@ -6,6 +6,33 @@ class CommandRegistry {
     this.client = client;
     this.commands = new Map();
     this.groups = new Map();
+
+    this.createGroup();
+  }
+
+  registerGroup(group) {
+    if (group instanceof CommandGroup) {
+      this.groups.set(group.id, group);
+      // console.log(`discord.js-commands| Group registered: ${group.id}`);
+    }
+  }
+
+  createGroup(options) {
+    const group = new CommandGroup(options);
+    this.registerGroup(group);
+  }
+
+  registerCommand(command) {
+    if (command instanceof Command) {
+      this.commands.set(command.id, command);
+      this.groups.get(command.options.group).commands.set(command.id, command);
+    }
+  }
+
+  createCommand(options, run) {
+    const command = new Command(options);
+    command.run = run;
+    this.registerCommand(command);
   }
 }
 
