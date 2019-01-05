4fc8fda363acf7d5d099994343fecf0377a679ba
SoggyPiggy <soggypiggy@gmail.com>
2019-01-05 15:33:20 -0500

    Update CommandGroup to use initialize intead of in the constructor

--- src/structures/commandgroup.js
+++ src/structures/commandgroup.js
@@ -5,24 +5,19 @@ const optionDefaults = {
 };
 
 class CommandGroup {
-  constructor(options = {}) {
-    this.options = { ...optionDefaults, ...options };
+  constructor(options) {
+    this.initialize(options);
     this.commands = new Map();
-
-    if (typeof this.options.id !== 'string') throw new Error('Command Group id must be a string');
-    if (typeof this.options.name !== 'string') this.options.name = this.options.id;
   }
 
-  get id() {
-    return this.options.id;
-  }
-
-  get name() {
-    return this.options.name;
-  }
-
-  get description() {
-    return this.options.description;
+  initialize(options = {}) {
+    const opts = { ...optionDefaults, ...options };
+    if (typeof opts.id !== 'string') throw new Error('Command Group id must be a string');
+    if (typeof opts.name !== 'string') opts.name = opts.id;
+    this.options = opts;
+    Object.keys(optionDefaults).forEach((key) => {
+      Object.defineProperty(this, key, { get: () => this.options[key] });
+    });
   }
 }
 
