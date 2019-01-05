3a1ec016b58c1389ec6e269f1a48eaf56511754a
SoggyPiggy <soggypiggy@gmail.com>
2019-01-05 15:41:12 -0500

    Update commandregistery

--- src/structures/commandregistry.js
+++ src/structures/commandregistry.js
@@ -1,36 +1,69 @@
-const { Command } = require('./command');
-const { CommandGroup } = require('./commandgroup');
+const { Command } = require('./Command');
+const { CommandGroup } = require('./CommandGroup');
+const { ParameterType } = require('./ParameterType');
+
+/* eslint-disable class-methods-use-this */
 
 class CommandRegistry {
   constructor(client) {
     this.client = client;
     this.commands = new Map();
-    this.groups = new Map();
+    this.commandGroups = new Map();
+    this.parameterTypes = new Map();
   }
 
-  registerGroup(group) {
-    if (group instanceof CommandGroup) {
-      this.groups.set(group.id, group);
-      // console.log(`discord.js-commands| Group registered: ${group.id}`);
-    }
+  registerDefaultCommands() {
+    // TODO: Add more default commands
   }
 
-  createGroup(options) {
-    const group = new CommandGroup(options);
-    this.registerGroup(group);
+  registerDefaultCommandGroups() {
+    // TODO: Add more default groups
+  }
+
+  registerDefaultParameterTypes() {
+    // TODO: Add more default parameterTypes
   }
 
   registerCommand(command) {
-    if (command instanceof Command) {
-      this.commands.set(command.id, command);
-      this.groups.get(command.options.group).commands.set(command.id, command);
-    }
+    if (!(command instanceof Command)) throw new Error('registerCommand only accepts instances of Command class');
+    // TODO: add functionality
   }
 
-  createCommand(options, run) {
-    const command = new Command(this, options);
-    command.run = run;
-    this.registerCommand(command);
+  registerCommandGroup(group) {
+    if (!(group instanceof CommandGroup)) throw new Error('registerGroup only accepts instance of CommandGroup class');
+    // TODO: add functionality
+  }
+
+  registerParameterType(type) {
+    if (!(type instanceof ParameterType)) throw new Error('registerType only accepts instance of ParameterType class');
+    // TODO: add functionality
+  }
+
+  quickCommand({ options, run }) {
+    this.registerCommand(new class extends Command {
+      constructor(registry) {
+        super(registry, options);
+        if (typeof run === 'function') this.run = run;
+      }
+    }(this));
+  }
+
+  quickCommandGroup({ options }) {
+    this.registerCommandGroup(new class extends CommandGroup {
+      constructor() {
+        super(options);
+      }
+    }());
+  }
+
+  quickParameterType({ options, parse, validate }) {
+    this.registerParameterType(new class extends ParameterType {
+      constructor() {
+        super(options);
+        if (typeof parse === 'function') this.parse = parse;
+        if (typeof validate === 'function') this.validate = validate;
+      }
+    }());
   }
 }
 
