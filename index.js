de7852828efb313072ac030ec604b5e50efec3c1
SoggyPiggy <soggypiggy@gmail.com>
2019-01-05 15:41:56 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client } = require('./src/Client');
+const { Command } = require('./src/structures/Command');
+const { CommandGroup } = require('./src/structures/CommandGroup');
+const { ParameterType } = require('./src/structures/ParameterType');
+
+module.exports = {
+  Client,
+  Command,
+  CommandGroup,
+  ParameterType,
+};
